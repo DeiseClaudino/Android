1237bea2f7853d87ad7e9536e3ab6a0060eff554
DeiseClaudino <deise.claudino98@gmail.com>
2018-07-16 23:09:14 -0300

    Com as alterações feitas, já é possível visualizar nome, telefone e foto dos alunos

--- app/src/main/java/br/com/alura/ageda/adapter/AlunosAdapter.java
+++ app/src/main/java/br/com/alura/ageda/adapter/AlunosAdapter.java
@@ -1,10 +1,13 @@
 package br.com.alura.ageda.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -51,6 +54,19 @@ public class AlunosAdapter extends BaseAdapter{
         
         TextView campoNome = (TextView) view.findViewById(R.id.item_nome);
         campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = (TextView) view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = (ImageView) view.findViewById(R.id.item_foto);
+        String caminhoFoto = aluno.getCaminhoFoto();
+        if (caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+
 
         return view;
     }
