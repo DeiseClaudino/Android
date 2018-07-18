10f12102f1b9a0bdf2f433294556d5b51a3cda15
DeiseClaudino <deise.claudino98@gmail.com>
2018-07-17 23:19:18 -0300

    Corrigindo crateFromPdu.

--- app/src/main/java/br/com/alura/ageda/receiver/SMSReceiver.java
+++ app/src/main/java/br/com/alura/ageda/receiver/SMSReceiver.java
@@ -3,18 +3,23 @@ package br.com.alura.ageda.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
+import br.com.alura.ageda.R;
 import br.com.alura.ageda.dao.AlunoDao;
 
 public class SMSReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
 
         Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
         byte[] pdu = (byte[]) pdus[0];
-        String[] formato = (String[]) intent.getSerializableExtra("format");
+        String formato = (String) intent.getSerializableExtra("format");
 
         SmsMessage sms = SmsMessage.createFromPdu(pdu, formato);
 
@@ -24,6 +29,8 @@ public class SMSReceiver extends BroadcastReceiver {
 
         if (dao.ehAluno(telefone)) {
             Toast.makeText(context, "Chegou um SMS!", Toast.LENGTH_SHORT).show();
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.msg);
+            mp.start();
         }
         dao.close();
     }
