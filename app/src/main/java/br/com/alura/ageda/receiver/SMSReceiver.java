0e1593e1c7b33c6a32939703d8307e0fca7a1622
DeiseClaudino <deise.claudino98@gmail.com>
2018-07-17 22:10:37 -0300

    Criando verificação que mostra se a mensagem recebida é de um aluno.

--- app/src/main/java/br/com/alura/ageda/receiver/SMSReceiver.java
+++ app/src/main/java/br/com/alura/ageda/receiver/SMSReceiver.java
@@ -3,11 +3,20 @@ package br.com.alura.ageda.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
 
 public class SMSReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
+        byte[] pdu = (byte[]) pdus[0];
+        String[] formato = (String[]) intent.getSerializableExtra("format");
+
+        SmsMessage sms = SmsMessage.createFromPdu(pdu, formato);
+
+        String telefone = sms.getPhone();
         Toast.makeText(context, "Chegou um SMS!", Toast.LENGTH_SHORT).show();
     }
 }
