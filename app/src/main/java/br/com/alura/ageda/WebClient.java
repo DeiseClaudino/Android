3943d30b282fb84f4772733a72e0da6429e39bd2
DeiseClaudino <deise.claudino98@gmail.com>
2018-07-18 22:52:39 -0300

    Classe WebClient finalizada, agora é necessário implementá-la na lista.

--- app/src/main/java/br/com/alura/ageda/WebClient.java
+++ app/src/main/java/br/com/alura/ageda/WebClient.java
@@ -8,19 +8,24 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class WebClient {
-    public void post(String json){
+    public String post(String json){
         try {
             URL url = new URL("https://www.caelum.com.br/mobile");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestProperty("Content-type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
             connection.setDoOutput(true);
 
            PrintStream output = new PrintStream(connection.getOutputStream());
            output.println(json);
 
             Scanner scanner = new Scanner(connection.getInputStream());
+
+            String resposta = scanner.next();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
         }
+        return null;
     }
 }
