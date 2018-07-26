6dfaef87b5078a125354b969fe4e741f1394c2c7
DeiseClaudino <deise.claudino98@gmail.com>
2018-07-25 22:26:37 -0300

    Criando novo Fragment para tela em Landscape.

---
+++ app/src/main/java/br/com/alura/ageda/DetalhesProvaFragment.java
@@ -0,0 +1,24 @@
+package br.com.alura.ageda;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetalhesProvaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.fragment_detalhes_prova, container, false);
+
+        return  view;
+    }
+
+
+}
