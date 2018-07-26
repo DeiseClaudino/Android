a470ce2e6e27025a512e6bf926e7e75afbc0ae6b
DeiseClaudino <deise.claudino98@gmail.com>
2018-07-25 21:55:06 -0300

    Finalizando criação do Fragment.

--- app/src/main/java/br/com/alura/ageda/ProvasActivity.java
+++ app/src/main/java/br/com/alura/ageda/ProvasActivity.java
@@ -21,27 +21,5 @@ public class ProvasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provas);
 
-        List<String> topicosPort = Arrays.asList("Sujeito", "Objeto direto", "Objeto indireto");
-        Prova provaPortugues = new Prova("Portugues", "25/06/2018", topicosPort);
-
-        List<String> topicosMat = Arrays.asList("Equações de segundo grau", "trigonometria");
-        Prova provaMatematica = new Prova("Matematica", "27/05/2016", topicosMat);
-
-        final List<Prova> provas = Arrays.asList(provaPortugues, provaMatematica);
-        ArrayAdapter<Prova> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, provas);
-
-        ListView lista =  findViewById(R.id.provas_lista);
-        lista.setAdapter(adapter);
-
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Prova prova = (Prova) parent.getItemAtPosition(position);
-                Toast.makeText(ProvasActivity.this, "Prova de " + prova, Toast.LENGTH_SHORT).show();
-                Intent vaiParaDetalhes = new Intent(ProvasActivity.this, DetalhesProvaActivity.class);
-                vaiParaDetalhes.putExtra("prova", prova);
-                startActivity(vaiParaDetalhes);
-            }
-        });
     }
 }
